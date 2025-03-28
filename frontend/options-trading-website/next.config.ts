c8d8a37ff4d7bbf1651f1882ef6396bfdeb518a4
nishantharcot <nsharc4@gmail.com>
2025-03-28 20:37:33 +0530

    fe urls checks in k8s

--- frontend/options-trading-website/next.config.ts
+++ frontend/options-trading-website/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://139.59.51.208:3000",
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL || "ws://144.126.253.177:8081"
+},
 };
 
 export default nextConfig;
