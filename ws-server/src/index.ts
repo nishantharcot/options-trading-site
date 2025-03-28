ce7ebbb2bf36907c8081c730ecdb74f51a6b6f68
nishantharcot <nsharc4@gmail.com>
2025-03-28 21:23:59 +0530

    webocket checking

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -1,7 +1,9 @@
 import { WebSocketServer } from "ws";
 import { UserManager } from "./UserManager";
 
-const wss = new WebSocketServer({ port: 8081 });
+const wss = new WebSocketServer({ host: "0.0.0.0", port: 8081 });
+
+console.log("ws load test log!")
 
 wss.on("connection", (ws) => {
   console.log("connection opened");
