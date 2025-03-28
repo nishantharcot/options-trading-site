e2408d02b36e7e5f12f24defb35d9562446dc01d
nishantharcot <nsharc4@gmail.com>
2025-03-28 16:19:12 +0530

    flow test in k8s

--- frontend/options-trading-website/src/utils/constants.ts
+++ frontend/options-trading-website/src/utils/constants.ts
@@ -1,2 +1,5 @@
+console.log("API_URL:- ", process.env.NEXT_PUBLIC_API_URL);
+console.log("WS_URL:-", process.env.NEXT_PUBLIC_WS_URL);
+
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || ""
 export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || ""
